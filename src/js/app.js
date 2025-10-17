3ef91c44e5d99fe9957f4352ed4df36397d29427
mohammad <mohammad@gmail.com>
2025-10-17 15:09:41 +0330

    real form

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,46 @@
-let form = document.querySelector(".user")
+let form = document.querySelector(".user");
 
-form.addEventListener("submit" , (e)=>{
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-let userData = {
-name:e.target.yame.value,
-age :e.target.age.value,
-email : e.target.eName.value,
-number : e.target.number.value,
-passworld : e.target.pFrom.value
-}
-for(let key in userData){
-    console.log(userData[key])
-}
-e.preventDefault()
-})
+  let userData = {
+    name: e.target.yame.value,
+    age: e.target.age.value,
+    email: e.target.eName.value,
+    number: e.target.number.value,
+    password: e.target.pFrom.value,
+  };
+
+ 
+  for (let key in userData) {
+    if (userData[key] === "") {
+      alert("لطفاً همه‌ی فیلدها را پر کنید!");
+      return;
+    }
+  }
+
+ 
+  if (userData.name.length < 3) {
+    alert("نام باید حداقل ۳ حرف باشد!");
+    return;
+  }
+
+  if (userData.age < 3) {
+    alert("سن کمتر از ۳ سال مجاز نیست!");
+    return;
+  }
+
+  if (userData.number.length < 7) {
+    alert("شماره باید حداقل ۷ رقم باشد!");
+    return;
+  }
+
+  if (userData.password.length < 6) {
+    alert("رمز عبور باید حداقل ۶ کاراکتر باشد!");
+    return;
+  }
 
 
+  console.log("اطلاعات کاربر:", userData);
+  alert("ثبت با موفقیت انجام شد ✅");
+});
