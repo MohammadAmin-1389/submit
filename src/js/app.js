32915d1a7e736bf431b412b6911639a2df917c13
mohammad <mohammad@gmail.com>
2025-11-03 10:59:30 +0330

    Array

--- src/js/app.js
+++ src/js/app.js
@@ -13,48 +13,11 @@ form.addEventListener("submit", (e) => {
     adress: e.target.adCod.value
   };
 
+  let datauser =[];
+  datauser.push(userData)
+  console.log(datauser);
  
-  for (let key in userData) {
-    if (userData[key] === "") {
-      alert("لطفاً همه‌ی فیلدها را پر کنید!");
-      return;
-    }
-  }
-
- 
-  if (userData.info.length < 3) {
-    alert("نام باید حداقل ۳ حرف باشد!");
-    return;
-  }
-
-  if (userData.age < 3) {
-    alert("سن کمتر از ۳ سال مجاز نیست!");
-    return;
-  }
-
-  if (userData.number.length < 7) {
-    alert("شماره باید حداقل ۷ رقم باشد!");
-    return;
-  }
-
-  if (userData.password.length < 6) {
-    alert("رمز عبور باید حداقل ۶ کاراکتر باشد!");
-    return;
-  }
-
-   if (userData.postCod.length < 7) {
-    alert("کد پستی حداقل هشت رقم است!")
-    ;
-    return;
-  }
-
-  if (userData.adCod.length < 5) {
-   alert("ادرست رو کامل بنویس");
-    return;
-  }
-
-
-  console.log("اطلاعات کاربر:", userData);
-  alert("ثبت با موفقیت انجام شد ✅");
-   e.preventDefault();
+e.preventDefault();
 });
+
+
